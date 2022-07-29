079ef1713febca63938753c35de818d006955c51
Maksym Sditanov <msditanov@200volts.com>
2022-07-29 22:15:36 +0300

    cmake: Move compile options to separate file

---
+++ cmake/CompileOptions.cmake
@@ -0,0 +1,22 @@
+if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    add_compile_options(
+            "-fno-elide-constructors"
+            "-Wall"
+            # fix for math/matrix.h ugly anonymous union
+            #"-pedantic-errors"
+            "-Wextra"
+            "-Winit-self"
+            "-Wold-style-cast"
+            "-Woverloaded-virtual"
+            "-Wuninitialized"
+            "-Wmissing-declarations"
+            "-Winit-self")
+endif()
+
+if (${FORCE_COLORED_OUTPUT})
+    if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+        add_compile_options (-fdiagnostics-color=always)
+    elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+        add_compile_options (-fcolor-diagnostics)
+    endif ()
+endif ()
